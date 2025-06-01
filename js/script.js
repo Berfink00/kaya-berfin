72e50cd2f602b64332ea9379fcd1499aeb734ada
Berfin Kaya <berfinkayaa123@gmail.com>
2025-06-01 22:41:34 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,30 @@
-const searchForm = document.querySelector(".search-form");Add commentMore actions
+const searchForm = document.querySelector(".search-form");
 const cartItem = document.querySelector(".cart-items-container");
 const navbar = document.querySelector(".navbar");
 
 //! buttons
-const searchBtn = document.querySelector("#search-btn");const searchBtn = document.querySelector("#search-btn");
+const searchBtn = document.querySelector("#search-btn");
 const cartBtn = document.querySelector("#cart-btn");
 const menuBtn = document.querySelector("#menu-btn");
 
-searchBtn.addEventListener("click", function () {searchBtn.addEventListener("click", function () {
-  searchForm.classList.toggle("active");  searchForm.classList.toggle("active");
+// Arama butonu
+searchBtn.addEventListener("click", function () {
+  searchForm.classList.toggle("active");
+
   document.addEventListener("click", function (e) {
     if (
-      !e.composedPath().includes(searchBtn) &&      !e.composedPath().includes(searchBtn) &&
-      !e.composedPath().includes(searchForm)      !e.composedPath().includes(searchForm)
+      !e.composedPath().includes(searchBtn) &&
+      !e.composedPath().includes(searchForm)
     ) {
-      searchForm.classList.remove("active");      searchForm.classList.remove("active");
+      searchForm.classList.remove("active");
     }
   });
 });
 
+// Sepet butonu
 cartBtn.addEventListener("click", function () {
   cartItem.classList.toggle("active");
+
   document.addEventListener("click", function (e) {
     if (
       !e.composedPath().includes(cartBtn) &&
@@ -31,8 +35,10 @@ cartBtn.addEventListener("click", function () {
   });
 });
 
+// Menü butonu
 menuBtn.addEventListener("click", function () {
   navbar.classList.toggle("active");
+
   document.addEventListener("click", function (e) {
     if (
       !e.composedPath().includes(menuBtn) &&
